2d0e888ae5b51c590001bf5298eab5c7f6142851
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-03-01 20:21:26 -0300

    commit
    Peso

--- aula01/index.js
+++ aula01/index.js
@@ -22,4 +22,24 @@ function drop(ev){
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
     
+
+var pesoTotal = 0;
+
+if(ev.target.id === "div2"){ 
+    pesoTotal += parseInt(event.target.childNodes [0].getAttribute("data-peso"));
+}else if(ev.target.id === "div1")
+    pesoTotal -= parseInt(event.target.childNodes [0].getAttribute("data-peso"));
+
+//exibir o peso total na página 
+document.getElementById("pesoTotal").innerHTML = pesoTotal + "KG"
+}
+
+function dragEnd(ev){
+    if(ev.target.parentNode.id != "div2"){
+        //subtarir o peso da imagem ao peso total
+        pesoTotal -= parseInt(ev.target.getAttribute("data-peso"));
+
+        //exibir o peso tptal na pagina
+        document.getElementById("pesoTotal").innerHTML = pesoTotal + "KG"
+    }
 }
